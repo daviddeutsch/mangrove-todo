1bca0f8a73dfc18d74f48e1aa96738c639e6ab84
David Deutsch <skore@valanx.org>
2014-01-28 16:49:20 +0100

    fixing hook service

--- pckg/mangrove-todo/lib/core/Services/HookService.php
+++ pckg/mangrove-todo/lib/core/Services/HookService.php
@@ -2,6 +2,22 @@
 
 class HookService extends AbstractService
 {
+	public function call( $method, $path, $data=null )
+	{
+		if ( !$this->is_callable($method) ) return null;
+
+		if ( $method == 'getUpdates' ) {
+			$p = explode('/', $path);
+			$s = array_pop($p);
+
+			return call_user_func( array($this, $method), $s );
+		} else {
+			call_user_func( array($this, $method), $data );
+
+			return null;
+		}
+	}
+
 	public function postSubscriber( $data )
 	{
 		RedBean_Pipeline::addSubscriber( $data );
