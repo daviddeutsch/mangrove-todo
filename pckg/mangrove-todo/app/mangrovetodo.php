b785d29949230276e621cb018e42d807166bf1ee
David Deutsch <skore@valanx.org>
2014-02-03 00:32:41 +0100

    new syntax for MangroveApp

--- pckg/mangrove-todo/app/mangrovetodo.php
+++ pckg/mangrove-todo/app/mangrovetodo.php
@@ -5,6 +5,6 @@ defined('_JEXEC') or die( 'Direct Access to this location is not allowed.' );
 
 require_once( dirname(__FILE__) . '/autoload.php' );
 
-MangroveTodoApp::init( dirname(__FILE__), 'mangrovetodo', array('todo') );
+MangroveTodoApp::add( dirname(__FILE__), 'mangrovetodo', array('todo') );
 
 MangroveTodoApp::start();
