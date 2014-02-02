d44c2c81ffc7d73b00d212f003063cd432a5bab0
David Deutsch <skore@valanx.org>
2014-02-02 13:18:00 +0100

    proper way to use this now

--- pckg/mangrove-todo/app/mangrovetodo.php
+++ pckg/mangrove-todo/app/mangrovetodo.php
@@ -5,6 +5,6 @@ defined('_JEXEC') or die( 'Direct Access to this location is not allowed.' );
 
 require_once( dirname(__FILE__) . '/autoload.php' );
 
-MangroveTodoApp::init( dirname(__FILE__), 'com_mangrovetodo', array('todo') );
+MangroveTodoApp::init( dirname(__FILE__), 'mangrovetodo', array('todo') );
 
 MangroveTodoApp::start();
